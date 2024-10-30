8c808e518a1860a8fe6e4b1844df54e8bcbece8e
kazanliev <kazanliev89@gmail.com>
2024-10-30 20:54:42 +0200

    formatted

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // console.log("DOMContentLoaded");
     const postalCodeInput = document.getElementById('postalCode');
     const cityInput = document.getElementById('city');
     const suggestionsContainer = document.getElementById('suggestions');
-    const userForm = document.getElementById("userForm");
 
     postalCodeInput.addEventListener('input', async () => {
         const postalCode = postalCodeInput.value;
 
+        // If the postal code has exactly 5 characters, fetch a suggestion for the city
         if (postalCode.length === 5) {
             try {
                 const response = await fetch(`https://api.zippopotam.us/de/${postalCode}`);
@@ -19,17 +18,17 @@ document.addEventListener('DOMContentLoaded', () => {
                 const data = await response.json();
                 const city = data.places[0]['place name'];
                 const suggestions = `<div class="suggestion-item">${postalCode}, ${city}</div>`;
-                
+
                 suggestionsContainer.innerHTML = suggestions;
                 suggestionsContainer.style.display = 'block';
 
-                // Add click event to the suggestion
+                // If clicked, fill the city input with the suggestion
                 const suggestionItems = document.querySelectorAll('.suggestion-item');
                 suggestionItems.forEach(item => {
                     item.addEventListener('click', () => {
                         cityInput.value = city;
                         suggestionsContainer.innerHTML = '';
-                        suggestionsContainer.style.display = 'none'; // Hide suggestions
+                        suggestionsContainer.style.display = 'none';
                     });
                 });
 
@@ -38,11 +37,11 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         } else {
             suggestionsContainer.innerHTML = '';
-            suggestionsContainer.style.display = 'none'; // Hide suggestions if input is less than 5
+            suggestionsContainer.style.display = 'none';
         }
     });
 
-    // Hide suggestions when clicking outside
+    // Hide the suggestion when clicking outside of the suggestion box
     document.addEventListener('click', (event) => {
         if (!postalCodeInput.contains(event.target) && !cityInput.contains(event.target)) {
             suggestionsContainer.innerHTML = '';
@@ -50,7 +49,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-    
+
 });
 
 
